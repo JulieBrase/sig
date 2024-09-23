8baa98588206370d3428353af4b2eb4c5cd61f00
Yuliya Kobrina <JulieBrase@gmail.com>
2024-09-23 21:28:45 +0300

    task done

---
+++ src/index.js
@@ -0,0 +1,30 @@
+class Character {
+    constructor(name, type, health, level, attack, defence) {
+        this.name = name;
+        this.type = type;
+        this.health = health;
+        this.level = level;
+        this.attack = attack;
+        this.defence = defence;
+    }
+}
+
+class Team {
+    constructor() {
+        this.characters = [];
+    }
+
+    addCharacter(character) {
+        if (character instanceof Character) {
+            this.characters.push(character);
+        } else {
+            throw new Error('Invalid character type');
+        }
+    }
+
+    *[Symbol.iterator]() {
+        for (const character of this.characters) {
+            yield character; // используем yield для выдачи каждого персонажа
+        }
+    }
+}
